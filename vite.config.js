1b58a081ec176944743176f8a58d4f6f264dace4
Krishnamoorthy.K <krishnamoorthyk.cse@gmail.com>
2026-01-01 12:29:13 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dashboard-blog-combined/', // 👈 IMPORTANT
+  base: '/dashboard-blog-combined/',
 })
